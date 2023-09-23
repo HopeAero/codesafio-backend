b8dba375925407b5c82b28d6bb90c858d98a0fde
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-09-23 04:58:55 -0400

    Admin protect

--- src/api/_routes/index.ts
+++ src/api/_routes/index.ts
@@ -5,6 +5,7 @@ import testRouter from '../test/test.routes'
 import userRouter from '../users/users.routes'
 import authRouter from '../auth/auth.routes'
 import publicationRouter from '../publication/publication.routes'
+import skillRouter from '../skill-categories/skill-categories.routes'
 import { tokenGuard } from '../../middlewares/tokenGuard'
 import { isAdmin, verifyToken } from '../../middlewares/auth'
 
@@ -13,6 +14,8 @@ const router = Router()
 router.use('/users', tokenGuard(), verifyToken(), isAdmin(), userRouter)
 router.use('/auth', authRouter)
 router.use('/publications', tokenGuard(), verifyToken(), publicationRouter)
+router.use('/skillCategory', tokenGuard(), verifyToken(), skillRouter)
+router.use('/skill', tokenGuard(), verifyToken(), skillRouter)
 router.use('/test', testRouter)
 
 export default router
--- src/api/skills/skills.routes.ts
+++ src/api/skills/skills.routes.ts
@@ -7,14 +7,15 @@ import { getSkillCategoryById } from './actions/getById.action'
 import { addSkill } from './actions/add.action'
 import { updateSkill } from './actions/update.action'
 import { deleteSkill } from './actions/delete.action'
+import { isAdmin } from '../../middlewares/auth'
 
 const router = Router()
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
 router.get('/', getSkills)
 router.get('/:skillId', getSkillCategoryById)
-router.post('/', schemaGuard(SkillSchema), addSkill)
-router.put('/:skillId', schemaGuard(SkillSchema), updateSkill)
-router.delete('/:skillId', deleteSkill)
+router.post('/', isAdmin(), schemaGuard(SkillSchema), addSkill)
+router.put('/:skillId', isAdmin(), schemaGuard(SkillSchema), updateSkill)
+router.delete('/:skillId', isAdmin(), deleteSkill)
 
 export default router
