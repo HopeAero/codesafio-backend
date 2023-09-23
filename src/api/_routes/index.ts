025d953b69fb7860ccecca637d4630a528ac96b8
Alejandro <alejandrojrosas2001@gmail.com>
2023-09-23 11:23:44 -0400

    fixing routes

--- src/api/_routes/index.ts
+++ src/api/_routes/index.ts
@@ -6,7 +6,6 @@ import skillsRouter from '../skills/skills.routes'
 import userRouter from '../users/users.routes'
 import authRouter from '../auth/auth.routes'
 import publicationRouter from '../publication/publication.routes'
-import skillRouter from '../skill-categories/skill-categories.routes'
 
 import { tokenGuard } from '../../middlewares/tokenGuard'
 import { isAdmin, verifyToken } from '../../middlewares/auth'
@@ -18,7 +17,5 @@ router.use('/skills', skillsRouter)
 router.use('/users', tokenGuard(), verifyToken(), isAdmin(), userRouter)
 router.use('/auth', authRouter)
 router.use('/publications', tokenGuard(), verifyToken(), publicationRouter)
-router.use('/skillCategory', tokenGuard(), verifyToken(), skillRouter)
-router.use('/skill', tokenGuard(), verifyToken(), skillRouter)
 
 export default router
