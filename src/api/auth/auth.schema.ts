1135361bb609c622acffa06d7e38d861adc23d04
Alejandro <alejandrojrosas2001@gmail.com>
2023-09-23 14:05:08 -0400

    wuu commit

--- src/api/auth/auth.schema.ts
+++ src/api/auth/auth.schema.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { Occupations } from '../../utils/roles.enum'
 
 export const loginSchema = z.object({
   email: z
@@ -13,13 +14,19 @@ export const loginSchema = z.object({
 })
 
 export const registerSchema = z.object({
+  name: z
+    .string()
+    .nonempty('Es necesario indicar un nombre de usuario')
+    .max(64, 'El nombre de usuario debe ser menos a 64 carácteres'),
   email: z
     .string()
     .nonempty('Es necesario indicar un correo electrónico')
-    .max(64, 'El nombre debe ser menor a 64 carácteres')
+    .max(64, 'El correo debe ser menor a 64 carácteres')
     .email('Debe ingresar un correo electrónico valido'),
   password: z
     .string()
     .nonempty('Es necesario ingresar una contraseña')
-    .max(64, 'El nombre debe ser menor a 64 carácteres')
+    .max(64, 'El nombre debe ser menor a 64 carácteres'),
+  occupation: z
+    .enum([Occupations.GRADUATED, Occupations.STUDENT, Occupations.TEACHER])
 })
