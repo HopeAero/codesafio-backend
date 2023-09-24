af609fc5c4841c515b265b1650b3dc6be7c1ea73
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-09-24 19:57:20 -0400

    Update

--- src/api/collaborators/actions/getPostulante.action.ts
+++ src/api/collaborators/actions/getPostulante.action.ts
@@ -40,7 +40,7 @@ export const getPostulent = async (
       TO_CHAR(a.created_at, 'DD/MM/YYYY - HH12:MI AM') AS created_at
   FROM applications AS a
   JOIN users AS u ON a.user_id = u.user_id
-  WHERE a.publication_id = 1 AND a.is_accepted IS NULL
+  WHERE a.publication_id = $1 AND a.is_accepted IS NULL
   ORDER BY a.publication_id;
   
         `,
